290c84c1a7aec75e6bbf279b2ac0ead7216aed75
xiaozhanqi <xiaozhanqi@example.com>
2026-05-19 17:32:27 +0000

    chore: 版本号更新为0.2.3-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.mimotts.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.2-beta"
+        versionCode = 7
+        versionName = "0.2.3-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
